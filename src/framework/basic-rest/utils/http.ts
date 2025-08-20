8ec12e80a789601b7dd1a240b749cc0dcf0bf4cc
Andreas Larsson <larssonandreas316@gmail.com>
2025-08-20 16:56:05 +0300

    update search

--- src/framework/basic-rest/utils/http.ts
+++ src/framework/basic-rest/utils/http.ts
@@ -3,7 +3,7 @@ import { getToken } from "./get-token";
 
 const http = axios.create({
   /*  baseURL: "https://natbutiken.lm.r.appspot.com", "http://localhost:4000" */
-  baseURL: "http://localhost:4000",
+  baseURL: "https://natbutiken.lm.r.appspot.com",
   timeout: 30000,
   headers: {
     Accept: "application/json",
